32298f65786ab43fb805217e6da25e2b8c8a95ab
testUser <testUser@email.com>
2021-03-08 10:25:35 +0800

    inital app

---
+++ smackproject/smackproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smackproject
+//
+//  Created by F_OS on 3/8/21.
+//  Copyright © 2021 F_OS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
