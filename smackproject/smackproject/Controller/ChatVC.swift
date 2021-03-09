94663716a8ca5f2935476736dfe230e5ccbef44d
testUser <testUser@email.com>
2021-03-09 18:01:42 +0800

    work on ui edit

---
+++ smackproject/smackproject/Controller/ChatVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChartVC.swift
+//  smackproject
+//
+//  Created by F_OS on 3/9/21.
+//  Copyright © 2021 F_OS. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ smackproject/smackproject/View/GradientView.swift
@@ -0,0 +1,15 @@
+//
+//  GradientView.swift
+//  smackproject
+//
+//  Created by F_OS on 3/9/21.
+//  Copyright © 2021 F_OS. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+
+
+}
